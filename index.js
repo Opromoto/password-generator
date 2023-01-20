70b5315b5a981468b5efcf7f7e030e2915bfd164
Opromoto <108427413+Opromoto@users.noreply.github.com>
2023-01-20 22:21:04 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+let pass1El = document.getElementById("pass1-el")
+let pass2El = document.getElementById("pass2-el")
+
+let nums = document.querySelector("input")
+
+let btnEl = document.getElementById("btn") 
+
+function random() {
+    let i = Math.floor(Math.random() * characters.length)
+    return characters[i]
+}
+
+function generate() {
+    pass1El.textContent = ""
+    pass2El.textContent = ""
+    
+    let num = Number(nums.value)
+    
+    for (let i = 0; i < num; i++) {
+        pass1El.textContent += random()
+        pass2El.textContent += random()
+    }
+    
+}
+
+
+btnEl.addEventListener("click", generate)
