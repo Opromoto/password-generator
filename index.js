b920fa60f51c37d890ff6e079c8203143be332c9
Opromoto <Prinububon@gmail.com>
2023-02-26 04:32:48 +0100

    trying to make it function

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"];
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"]
 
-let pass1El = document.getElementById("pass1-el")
-let pass2El = document.getElementById("pass2-el")
+const pass1El = document.getElementById("pass1-el")
+const pass2El = document.getElementById("pass2-el")
 
-let nums = document.querySelector("input")
+const nums = document.querySelector("input")
 
-let btnEl = document.getElementById("btn") 
+const btnEl = document.getElementById("btn") 
 
 function random() {
     let i = Math.floor(Math.random() * characters.length)
